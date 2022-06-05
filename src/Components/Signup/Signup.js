27c695acf8bcf58de336901726b617fb658fac73
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-06-05 08:49:41 +0400

    Signup -username,email,phone, password take the data and pass to state

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Logo from '../../olx-logo.png';
 import './Signup.css';
 
 export default function Signup() {
+  //now im going to take the user data from user using this page .and i will create state for that using that state i will pass the state into firebase. then i will render the authenticate the signup using that state .so i will crete a state now
+  const [username,setUsername]=useState('')//i pass the data as a string using ('')
+  // i will pass the all the data using setState
+  const [email,setEmail] = useState('');// 
+  const [phone,setPhone] =useState('');
+  const [password,setPassword] =useState('')
   return (
     <div>
       <div className="signupParentDiv">
@@ -14,6 +20,8 @@ export default function Signup() {
           <input
             className="input"
             type="text"
+            value={username}//i pass the state here
+            onChange={(e)=>setUsername(e.target.value)}//this (e) is representing event.  (e.target.value)} means take that value from onchange
             id="fname"
             name="name"
             defaultValue="John"
@@ -24,6 +32,8 @@ export default function Signup() {
           <input
             className="input"
             type="email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
             id="fname"
             name="email"
             defaultValue="John"
@@ -34,6 +44,8 @@ export default function Signup() {
           <input
             className="input"
             type="number"
+            value={phone}
+            onChange={(e)=>setPhone(e.target.value)}
             id="lname"
             name="phone"
             defaultValue="Doe"
@@ -44,6 +56,8 @@ export default function Signup() {
           <input
             className="input"
             type="password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
             id="lname"
             name="password"
             defaultValue="Doe"
