65130a31a673e24e6d09d7aca01da57ddbf4026d
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-06-05 09:12:19 +0400

    signup- set up firebase config succesully

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+//take the sdk and configuration from firebase.com>project settings>config  then copy the data
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBGG9BL4osat3wRSbcdkR_AkVBR6PzVrgE",
+    authDomain: "olx-demo-e1568.firebaseapp.com",
+    projectId: "olx-demo-e1568",
+    storageBucket: "olx-demo-e1568.appspot.com",
+    messagingSenderId: "836762922355",
+    appId: "1:836762922355:web:cda19b59a24527d9326906",
+    measurementId: "G-1Y80YRHFS3"
+  };
+
+  // then need pass the firebase and export it save it as a variable
+export const Firebase= firebase.initializeApp(firebaseConfig)
